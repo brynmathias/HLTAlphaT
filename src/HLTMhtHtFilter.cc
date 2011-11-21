cb8efff7d64f2ae4705b133d3036251c6a9d17f2
Bryn Mathias <brynmathias@gmail.com>
2011-11-21 18:41:19 +0100

    try with Lvec

--- src/HLTMhtHtFilter.cc
+++ src/HLTMhtHtFilter.cc
@@ -6,7 +6,7 @@
 */
 
 #include "HLTrigger/JetMET/interface/HLTMhtHtFilter.h"
-// #include "HLTrigger/JetMET/interface/AlphaT.hh"
+#include "HLTrigger/JetMET/interface/AlphaT.hh"
 
 #include "DataFormats/Common/interface/Handle.h"
 
@@ -58,9 +58,9 @@ HLTMhtHtFilter::HLTMhtHtFilter(const edm::ParameterSet& iConfig)
 
   // sanity checks
   if (       (minPtJet_.size()    !=  etaJet_.size())
-       || (  (minPtJet_.size()<1) || (etaJet_.size()<1) )
-       || ( ((minPtJet_.size()<2) || (etaJet_.size()<2))
-      && ( (mode_==1) || (mode_==2) || (mode_ == 5))) ) {
+    || (  (minPtJet_.size()<1) || (etaJet_.size()<1) )
+    || ( ((minPtJet_.size()<2) || (etaJet_.size()<2))
+  && ( (mode_==1) || (mode_==2) || (mode_ == 5))) ) {
     edm::LogError("HLTMhtHtFilter") << "inconsistent module configuration!";
   }
 
@@ -147,7 +147,7 @@ bool
           ht += jetVar;
           nj++;
           LorentzV JetLVec(recocalojet->pt(),recocalojet->eta(),recocalojet->phi(),recocalojet->mass());
-          // JetLVec.SetPtEtaPhiM
+
           jets.push_back( JetLVec );
         }
       }
@@ -163,39 +163,7 @@ bool
         double mHT = sqrt( (mhtx*mhtx) + (mhty*mhty) );
         dht += ( nj < 2 ? jetVar : -1.* jetVar ); //@@ only use for njets < 4
 
-        std::vector<double> et;
-        std::vector<double> px;
-        std::vector<double> py;
-
-        double alphaT = 0.;
-        transform( jets.begin(), jets.end(), back_inserter(et), std::mem_fun_ref(&LorentzV::Et) );
-        transform( jets.begin(), jets.end(), back_inserter(px), std::mem_fun_ref(&LorentzV::Px) );
-        transform( jets.begin(), jets.end(), back_inserter(py), std::mem_fun_ref(&LorentzV::Py) );
-
-
-
-        const double sum_et = accumulate( et.begin(), et.end(), 0. );
-        const double sum_px = accumulate( px.begin(), px.end(), 0. );
-        const double sum_py = accumulate( py.begin(), py.end(), 0. );
-
-    double min_delta_sum_et = -1.;
-    for ( unsigned i=0; i < unsigned(1<<(et.size()-1)); i++ ) { //@@ iterate through different combinations
-      double delta_sum_et = 0.;
-      std::vector<bool> jet;
-      for ( unsigned j=0; j < et.size(); j++ ) { //@@ iterate through jets
-        delta_sum_et += et[j] * ( 1 - 2 * (int(i>>j)&1) );
-
-      }
-      if ( ( fabs(delta_sum_et) < min_delta_sum_et || min_delta_sum_et < 0. ) ) {
-        min_delta_sum_et = fabs(delta_sum_et);
-      }
-    }
-    if ( min_delta_sum_et < 0. ) { alphaT =  0.; }
-
-    // Alpha_T
-     alphaT =  ( 0.5 * ( sum_et - min_delta_sum_et ) / sqrt( sum_et*sum_et - (sum_px*sum_px+sum_py*sum_py) ) );
-
-
+        alphaT = AlphaT()( jets );
         if ( nj == 2 || nj == 3 ) {
           aT = ( ht - fabs(dht) ) / ( 2. * sqrt( ( ht*ht ) - ( mHT*mHT  ) ) );
         } else if ( nj > 3 ) {
@@ -209,32 +177,32 @@ bool
       }
     }
 
-  if( mode_==1 && sqrt(mhtx*mhtx + mhty*mhty) > minMht_) flag=1;
-  if( mode_==2 && sqrt(mhtx*mhtx + mhty*mhty)+ht > minMeff_) flag=1;
-  if( mode_==3 && sqrt(mhtx*mhtx + mhty*mhty) > minPT12_ && nj>1) flag=1;
-  if( mode_==4 && ht > minHt_) flag=1;
+    if( mode_==1 && sqrt(mhtx*mhtx + mhty*mhty) > minMht_) flag=1;
+    if( mode_==2 && sqrt(mhtx*mhtx + mhty*mhty)+ht > minMeff_) flag=1;
+    if( mode_==3 && sqrt(mhtx*mhtx + mhty*mhty) > minPT12_ && nj>1) flag=1;
+    if( mode_==4 && ht > minHt_) flag=1;
 
-  if (flag==1) {
-    for (reco::CaloJetCollection::const_iterator recocalojet = recocalojets->begin(); recocalojet!=recocalojets->end(); recocalojet++) {
-      jetVar = recocalojet->pt();
-      if (!usePt_ || mode_==3) jetVar = recocalojet->et();
+    if (flag==1) {
+      for (reco::CaloJetCollection::const_iterator recocalojet = recocalojets->begin(); recocalojet!=recocalojets->end(); recocalojet++) {
+        jetVar = recocalojet->pt();
+        if (!usePt_ || mode_==3) jetVar = recocalojet->et();
 
-      if (jetVar > minPtJet_.at(0)) {
-        ref = CaloJetRef(recocalojets,distance(recocalojets->begin(),recocalojet));
-        filterobject->addObject(TriggerJet,ref);
-        n++;
+        if (jetVar > minPtJet_.at(0)) {
+          ref = CaloJetRef(recocalojets,distance(recocalojets->begin(),recocalojet));
+          filterobject->addObject(TriggerJet,ref);
+          n++;
+        }
       }
     }
-  }
-} // events with at least one jet
+  } // events with at least one jet
 
 
 
   // filter decision
-bool accept(n>0);
+  bool accept(n>0);
 
   // put filter object into the Event
-iEvent.put(filterobject);
+  iEvent.put(filterobject);
 
-return accept;
+  return accept;
 }
